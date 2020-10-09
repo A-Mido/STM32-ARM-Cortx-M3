be0f1d222dfa278da7d7360a452a333830470f50
Ahmed Sayed <a7med.skf@gmail.com>
2020-10-09 20:27:33 +0200

    Feat: adding basic standard data types and functions these two files will be used widely.

---
+++ 01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -0,0 +1,21 @@
+/***********************************************/
+/*        Author: Ahmed Sayed                  */
+/*        Date: 06-Sep-2020                    */
+/*        Version: V01                         */
+/***********************************************/
+
+#ifndef _STD_TYPES_H
+#define _STD_TYPES_H
+
+typedef unsigned char 		u8;
+typedef unsigned short int u16;
+typedef unsigned int	   u32;
+typedef unsigned long int  u64;
+
+typedef char	  s8;
+typedef short int s16;
+typedef int       s32;
+typedef long int  s64;
+
+
+#endif
